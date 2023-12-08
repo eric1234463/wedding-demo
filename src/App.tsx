66d688c7af5ed977c941f181f768562cc3f66bd9
Eric Kwong <eric1234463@gmail.com>
2023-12-08 22:07:41 +0800

    fix: add ogimage

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import WeddingImageSlider from "./Components/ImageSlider"
 import InvitationCard from "./Components/InvitationCard"
+import OgImage from "../src/assets/ogImage.jpg"
 
 function App() {
   return (
-    <div className='w-full grid grid-cols-12 h-full lg:h-screen'>
-      <div className="lg:col-span-7 col-span-12">
-        <WeddingImageSlider />
+    <>
+      <meta property="og:image" content={OgImage}></meta>
+      <div className='w-full grid grid-cols-12 h-full lg:h-screen'>
+        <div className="lg:col-span-7 col-span-12">
+          <WeddingImageSlider />
+        </div>
+        <div className="lg:col-span-5 col-span-12 ">
+          <InvitationCard />
+        </div>
       </div>
-      <div className="lg:col-span-5 col-span-12 ">
-        <InvitationCard />
-      </div>
-    </div>
+    </>
   )
 }
 
