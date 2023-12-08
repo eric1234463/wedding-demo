fddd0b1a38d645145d0f18b16ae6e6142474a934
Eric Kwong <eric1234463@gmail.com>
2023-12-09 00:04:17 +0800

    feat: update photo

--- src/Components/ImageSlider.tsx
+++ src/Components/ImageSlider.tsx
@@ -3,6 +3,11 @@ import 'react-slideshow-image/dist/styles.css'
 import slider1 from '../assets/slider-1.jpg'
 import slider2 from '../assets/slider-2.jpg'
 import slider3 from '../assets/slider-3.jpg'
+import slider4 from '../assets/slider-4.jpg'
+import slider5 from '../assets/slider-5.jpg'
+import slider6 from '../assets/slider-6.jpg'
+import slider7 from '../assets/slider-7.jpg'
+import slider8 from '../assets/slider-8.jpg'
 
 const slideImages = [
   {
@@ -17,18 +22,33 @@ const slideImages = [
     url: slider3,
     description: "Thanks For Coming",
   },
+  {
+    url: slider4,
+  },
+  {
+    url: slider5,
+  },
+  {
+    url: slider6,
+  },
+  {
+    url: slider7,
+  },
+  {
+    url: slider8,
+  },
 ];
 
 const WeddingImageSlider = () => {
   return (
     <div className="slide-container lg:h-screen h-full">
-      <Slide arrows={false} duration={5000}>
+      <Slide arrows={false} duration={4000} autoplay={true}>
         {slideImages.map((slideImage, index) => (
           <div key={index}>
             <div style={{ 'backgroundImage': `url(${slideImage.url})` }} className='bg-cover bg-center h-screen relative'>
-              <div className='absolute bottom-0 left-0 p-32 text-[#fff]'>
-                <div className='text-6xl pb-16'>Cynthia & Eric</div>
-                <div className='text-4xl font-rainbow'>{slideImage.description}</div>
+              <div className='absolute top-0 left-0 p-16 text-[#E8E8E8] lg:p-32'>
+                <div className='font-bold text-3xl lg:text-6xl mb-4 lg:mb-16'>Cynthia & Eric</div>
+                {slideImage.description && <div className='font-bold text-xl lg:text-4xl font-rainbow'>{slideImage.description}</div>}
               </div>
             </div>
           </div>
