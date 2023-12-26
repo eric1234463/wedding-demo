113fbeabbb52ca5483b74d0a963f208995902b4a
Eric Kwong <eric1234463@gmail.com>
2023-12-26 14:13:06 +0800

    feat: payment text

--- src/pages/PaymentPage.tsx
+++ src/pages/PaymentPage.tsx
@@ -1,16 +1,17 @@
 import payme from '../assets/payme.jpg'
 import fps from '../assets/fps.jpg'
 import alipay from '../assets/alipay.jpg'
+import backgroundImage from '../assets/payment-bg.jpg'
 
 const PaymentPage = () => {
   return (
-    <div className='px-4 py-16'>
-      <div className='text-center font-bold text-3xl lg:text-6xl mb-4 lg:mb-16'>Cynthia & Eric</div>
-      <div className='text-center font-bold text-xl lg:text-3xl mb-4 lg:mb-16'>21 Jan 2024</div>
-      <div className='grid grid-cols-3 h-[600px]'>
-        <div style={{ 'backgroundImage': `url(${payme})` }} className='bg-cover bg-center h-full relative'/>
-        <div style={{ 'backgroundImage': `url(${fps})` }} className='bg-cover bg-center h-full relative'/>
-        <div style={{ 'backgroundImage': `url(${alipay})` }} className='bg-cover bg-center h-full relative' />
+    <div style={{ 'backgroundImage': `url(${backgroundImage})` }} className='px-4 py-16 bg-cover bg-center h-screen relative'>
+      <div className='text-center font-bold text-3xl lg:text-6xl mb-4 lg:mb-16 text-[#fe6694]'>Cynthia & Eric</div>
+      <div className='text-center font-bold text-xl lg:text-3xl mb-4 lg:mb-16 text-[#fe6694]'>21 Jan 2024</div>
+      <div className='grid grid-cols-3 h-[400px] gap-2'>
+        <div style={{ 'backgroundImage': `url(${payme})` }} className='bg-contain bg-center h-full relative'/>
+        <div style={{ 'backgroundImage': `url(${fps})` }} className='bg-contain bg-center h-full relative'/>
+        <div style={{ 'backgroundImage': `url(${alipay})` }} className='bg-contain bg-center h-full relative' />
       </div>
     </div>
   )
