3d02f4991234753ffce0333be4caf6afa787846b
Eric Kwong <eric1234463@gmail.com>
2024-01-19 23:05:22 +0800

    chore: update image slider

--- src/components/ImageSlider.tsx
+++ src/components/ImageSlider.tsx
@@ -22,6 +22,26 @@ import slider19 from '../assets/19.jpg'
 import slider20 from '../assets/20.jpg'
 import slider21 from '../assets/21.jpg'
 import slider22 from '../assets/22.jpg'
+import slider23 from '../assets/23.jpg'
+import slider24 from '../assets/24.jpg'
+import slider25 from '../assets/25.jpg'
+import slider26 from '../assets/26.jpg'
+import slider27 from '../assets/27.jpg'
+import slider28 from '../assets/28.jpg'
+import slider29 from '../assets/29.jpg'
+import slider30 from '../assets/30.jpg'
+import slider31 from '../assets/31.jpg'
+import slider32 from '../assets/32.jpg'
+import slider33 from '../assets/33.jpg'
+import slider34 from '../assets/34.jpg'
+import slider35 from '../assets/35.jpg'
+import slider36 from '../assets/36.jpg'
+import slider37 from '../assets/37.jpg'
+import slider38 from '../assets/38.jpg'
+import slider39 from '../assets/39.jpg'
+import slider40 from '../assets/40.jpg'
+import slider41 from '../assets/41.jpg'
+import slider42 from '../assets/42.jpg'
 
 const slideImages = [
   {
@@ -78,7 +98,67 @@ const slideImages = [
     url: slider21,
   }, {
     url: slider22,
-  }
+  },
+  {
+    url: slider23,
+  },
+  {
+    url: slider24,
+  },
+  {
+    url: slider25,
+  },
+  {
+    url: slider26,
+  },
+  {
+    url: slider27,
+  },
+  {
+    url: slider28,
+  },
+  {
+    url: slider29,
+  },
+  {
+    url: slider30,
+  },
+  {
+    url: slider31,
+  },
+  {
+    url: slider32,
+  },
+  {
+    url: slider33,
+  },
+  {
+    url: slider34,
+  },
+  {
+    url: slider35,
+  },
+  {
+    url: slider36,
+  },
+  {
+    url: slider37,
+  },
+  {
+    url: slider38,
+  },
+  {
+    url: slider39,
+  },
+  {
+    url: slider40,
+  },
+  {
+    url: slider41,
+  },
+  {
+    url: slider42,
+  },
 ];
 
 const WeddingImageSlider = () => {
